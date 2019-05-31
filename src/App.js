a9782cb56b8889de9748be43506bc850f8ae0865
Kenneth Ashley <ken.u.diggitt@gmail.com>
2019-05-30 22:36:48 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <div className="spacer"></div>
+    </div>
+  );
+}
+
+export default App;
